491b4709c3d9fc09fe9c932d5214208c1f89ccb6
pandu1031 <chintupandu1031@gmail.com>
2023-09-07 20:58:17 +0530

    5th commit

--- BuildDeploy.groovy
+++ BuildDeploy.groovy
@@ -31,7 +31,7 @@ pipeline{
         stage("deploy"){
             steps{
                 println "here im deploying the war file to tomcat server"
-                +sh "scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem /tmp/tomcatinstall.sh ec2-user@${SERVER_IP}: /tmp/"
+                sh "scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem /tmp/tomcatinstall.sh ec2-user@${SERVER_IP}: /tmp/"
                 sh "ssh -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem ec2-user@${SERVER_IP} \"bash /tmp/tomcatinstall.sh systemctl status tomcat\""
                  sh "scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps/"
             

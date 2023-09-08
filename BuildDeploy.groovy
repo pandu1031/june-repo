e9f083f9ab516f6d6ac6d0f91efe246fc5e6f75d
pandu1031 <chintupandu1031@gmail.com>
2023-09-08 15:02:13 +0530

    8th commit

--- BuildDeploy.groovy
+++ BuildDeploy.groovy
@@ -50,7 +50,7 @@ pipeline{
             echo "$ip"
             aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuuu/${BRANCH_NAME}/${BUILD_NUMBER}
             scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@172.31.5.221:/var/lib/tomcat/webapps
-            scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@72.31.13.5:/var/lib/tomcat/webapps
+            scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@172.31.13.5:/var/lib/tomcat/webapps
             
             done
             '''

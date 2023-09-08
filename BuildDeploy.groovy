a2af3ab94fc5db0edcebd5b4c409cc910796c907
pandu1031 <chintupandu1031@gmail.com>
2023-09-08 14:06:40 +0530

    8th commit

--- BuildDeploy.groovy
+++ BuildDeploy.groovy
@@ -40,18 +40,18 @@ pipeline{
                 scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@172.31.5.221:/var/lib/tomcat/webapps/
                 scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem target/hello-${BUILD_NUMBER}.war ec2-user@172.31.13.5:/var/lib/tomcat/webapps/ """
             
-                 sh """
-                 aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuuu/${BRANCH_NAME}/${BUILD_NUMBER}
+                 
+               sh"""  aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuuu/${BRANCH_NAME}/${BUILD_NUMBER}
                  ls -l 
-            whoami
+            whoami """
             IFS=',' read -ra storevalue <<< "${SERVER_IP}"
-            for ip in ${storevalue[@]}
+            for ip in "${storevalue[@]}"
             do
             echo "$ip"
-            "scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem /mamuuu/source/${BUILD_NUMBER}/hello-${BUILD_NUMBER}.war ec2-user@$ip:/var/lib/tomcat/webapps"
-            ssh -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem ec2-user@$ip "hostname"
+           sh "scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem /mamuuu/source/${BUILD_NUMBER}/hello-${BUILD_NUMBER}.war ec2-user@$ip:/var/lib/tomcat/webapps"
+           sh "ssh -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem ec2-user@$ip "hostname" "
             done
-            """
+            
             }
         }
     }
